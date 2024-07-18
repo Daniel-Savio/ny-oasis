9492fdf2df2c82f3e3e2b772894996342dd59035
Daniel Sávio <spnjc137@gmail.com>
2024-07-17 21:29:24 -0300

    Build: Adding and configuring shadcnui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div className="bg-orange-400">Hey There</div>
+      <Button>Daniel</Button>
     </>
   );
 }
